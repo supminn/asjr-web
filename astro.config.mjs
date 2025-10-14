1d25fa7d3e854353d9498928b0f7d8b7a1d918a2
supminn <sansup49@gmail.com>
2025-10-14 21:07:06 +0530

    feat: actions workflow to deploy pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://asjr.github.io', // Or 'https://your-username.github.io/your-repo-name'
+  base: '/asjr-web', // Only if deploying to a project page (e.g., your-username.github.io/your-repo-name)
+  output: 'static',
+  outDir: './docs',
+});
