8f519c0ca9b73be3ee91682ab33932c4b2cf3d8a
Deeya Upadhyay <58636958+2020Deeya@users.noreply.github.com>
2025-11-19 11:33:05 +0530

    feat: refine about us page (#81)
    * feat: refine about us page
    
    * address review comments

--- src/i18n/get.ts
+++ src/i18n/get.ts
@@ -2,14 +2,16 @@ import enHeader from './en/header.json';
 import esHeader from './es/header.json';
 import enIndex from './en/index.json';
 import esIndex from './es/index.json';
+import enAbout from './en/about.json';
+import esAbout from './es/about.json';
 import enWhatWeDo from './en/what-we-do.json';
 import esWhatWeDo from './es/what-we-do.json';
 import enLatestUpdates from './en/latest-updates.json';
 import esLatestUpdates from './es/latest-updates.json';
 
 export const translations = {
-  en: { ...enHeader, ...enIndex, ...enWhatWeDo, ...enLatestUpdates } as Record<string, string>,
-  es: { ...esHeader, ...esIndex, ...esWhatWeDo, ...esLatestUpdates } as Record<string, string>,
+  en: { ...enHeader, ...enIndex, ...enAbout, ...enWhatWeDo, ...enLatestUpdates } as Record<string, string>,
+  es: { ...esHeader, ...esIndex, ...esAbout, ...esWhatWeDo, ...esLatestUpdates } as Record<string, string>,
 };
 
 export type Lang = keyof typeof translations;
